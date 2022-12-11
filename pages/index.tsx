1012bc4d4b859e3d01be5abdb54bf5488c5b6a39
Andrés Castillo <andres.castillo@acastillo.co>
2022-12-11 03:31:58 -0500

    feat: change logo and add cursor animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,13 @@ export default function Home() {
             <div className="logo flex justify-between items-end">
               <div className="p-2 m-4 border-4 border-rich-black flex">
                 <h1 className="text-3xl font-bold p-3 bg-rich-black text-maize-crayola">
-                  _ACastillo
+                  ACastillo
+                  <span className="animate-[terminal-cursor_1s_infinite]">
+                    _
+                  </span>
                 </h1>
               </div>
-              <h2 className="mb-4 font-bold text-xl mr-4	">Web Developer</h2>
+              <h2 className="mb-4 font-bold text-xl mr-4">Web Developer</h2>
             </div>
           </div>
         </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
         'rich-black': '#070707',
         'maize-crayola': '#EEC643',
+      },
+      keyframes: {
+        'terminal-cursor': {
+          '0%': {
+            opacity: 0,
+          },
+          '40%': {
+            opacity: 0,
+          },
+          '50%': {
+            opacity: 1,
+          },
+          '90%': {
+            opacity: 1,
+          },
+          '100%': {
+            opacity: 0,
+          },
+        },
       },
     },
   },
